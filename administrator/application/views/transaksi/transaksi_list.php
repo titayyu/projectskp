c2f0cc51759c692e2340fd251be48927d7c184cd
habibaturohmah14 <76829968+habibaturohmah14@users.noreply.github.com>
2021-01-03 13:33:15 +0700

    Update transaksi_list.php
    update8

--- administrator/application/views/transaksi/transaksi_list.php
+++ administrator/application/views/transaksi/transaksi_list.php
@@ -59,6 +59,7 @@
                       </td>
           </thead>	    
         </table>
+
 <script src="<?php echo base_url('assets/js/jquery-1.11.2.min.js') ?>"></script>
         <script src="<?php echo base_url('assets/datatables/jquery.dataTables.js') ?>"></script>
         <script src="<?php echo base_url('assets/datatables/dataTables.bootstrap.js') ?>"></script>
@@ -77,4 +78,3 @@
                     };
                 };          
         </script>
-    <!-- // Tampil Data transaksi -->
