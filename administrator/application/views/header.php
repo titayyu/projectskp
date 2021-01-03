c74fc381b97a19a7d56b693c349898e584032958
Tita Ayu R <47605688+titayyu@users.noreply.github.com>
2021-01-03 11:09:38 +0700

    Update header.php
    #4

--- administrator/application/views/header.php
+++ administrator/application/views/header.php
@@ -36,9 +36,9 @@
     <!-- Logo -->
     <a href="<?php echo base_url('admin') ?>" class="logo">
       <!-- mini logo for sidebar mini 50x50 pixels -->
-      <span class="logo-mini"><b>Tita</b>Jaya</span>
+      <span class="logo-mini"><b>POLI</b>PANGKEP</span>
       <!-- logo for regular state and mobile devices -->
-      <span class="logo-lg"><b>CRM</b></span>
+      <span class="logo-lg"><b>E-KINERJA</b></span>
     </a>
     <!-- Header Navbar: style can be found in header.less -->
     <nav class="navbar navbar-static-top">
