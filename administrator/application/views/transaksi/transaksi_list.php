c8587c672043de3199337dd7e8eec0dfab6c904c
habibaturohmah14 <76829968+habibaturohmah14@users.noreply.github.com>
2021-01-03 12:48:19 +0700

    Update transaksi_list.php
    update2

--- administrator/application/views/transaksi/transaksi_list.php
+++ administrator/application/views/transaksi/transaksi_list.php
@@ -1,25 +1,24 @@
-
 <section class="content-header">
       <h1>
-        E-KINERJA SKP UHUY2
+        E-KINERJA
         <small>Politeknik Negeri Pangkajene Kepulauan</small>
       </h1>
       <ol class="breadcrumb">
-        <li><a href="admin"><i class="fa fa-dashboard"></i> Home</a></li>
-        <li class="active">Transaksi</li>
+        <li><a href="admin"><i class="fa fa-dashboard"></i>Home</a></li>
+        <li class="active">SKP</li>
       </ol>
     </section>
+    
     <!-- Main content -->
     <section class="content">
-
-      <!-- Default box -->
-      <div class="box">        
-        <div class="box-body">
-		
+    
+    <!-- Default box -->
+    <div class="box">        
+        <div class="box-body">		
 		<!-- Tampil Data transaksi -->
         <div class="row" style="margin-bottom: 10px">
             <div class="col-md-4">
-                <h2 style="margin-top:0px">Transaksi </h2>
+                <h2 style="margin-top:0px"></h2>
             </div>
             <div class="col-md-4 text-center">
                 <div style="margin-top: 4px"  id="message">
@@ -27,22 +26,41 @@
                 </div>
             </div>
             <div class="col-md-4 text-right">
-                <?php echo anchor(site_url('transaksi/create'), 'Create', 'class="btn btn-primary"'); ?>
 	    </div>
         </div>
-        <table class="table table-bordered table-striped" id="mytable">
+        
+        <table class="table table-bordered table-striped" id="mytable"><center>
             <thead>
                 <tr>
-                    <th width="80px">No</th>
-                    <th>ID Transaksi</th>	
-                    <th>ID Pelanggan</th>	
-                    <th>Tanggal</th>
-					<th width="200px">Action</th>
+                    <th><center>No</th>
+                    <th><center>Tahun Akademik</th>	
+					<th><center>Action</th>
                 </tr>
             </thead>
-	    
+
+            <thead class="">
+                      <td><center>1</td>
+                      <td><center>2019</td>
+                      <td class="col-md-2"><center>
+                          <a class="btn btn-social-icon btn-primary" href=""><i class="fa fa-eye"></i></a>  
+                      </td>
+            </thead>
+            <thead class="">
+                      <td><center>2</td>
+                      <td><center>2020</td>
+                      <td class="col-md-2"><center>
+                          <a class="btn btn-social-icon btn-primary" href=""><i class="fa fa-eye"></i></a>  
+                      </td>
+          </thead>
+          <thead class="">
+                      <td><center>3</td>
+                      <td><center>2021</td>
+                      <td class="col-md-2"><center>
+                        <?php echo anchor(site_url('transaksi/create'), 'Create', 'class="btn btn-primary"'); ?>
+                      </td>
+          </thead>	    
         </table>
-        <script src="<?php echo base_url('assets/js/jquery-1.11.2.min.js') ?>"></script>
+<script src="<?php echo base_url('assets/js/jquery-1.11.2.min.js') ?>"></script>
         <script src="<?php echo base_url('assets/datatables/jquery.dataTables.js') ?>"></script>
         <script src="<?php echo base_url('assets/datatables/dataTables.bootstrap.js') ?>"></script>
         <script type="text/javascript">
@@ -58,48 +76,6 @@
                         "iPage": Math.ceil(oSettings._iDisplayStart / oSettings._iDisplayLength),
                         "iTotalPages": Math.ceil(oSettings.fnRecordsDisplay() / oSettings._iDisplayLength)
                     };
-                };
-
-                var t = $("#mytable").dataTable({
-                    initComplete: function() {
-                        var api = this.api();
-                        $('#mytable_filter input')
-                                .off('.DT')
-                                .on('keyup.DT', function(e) {
-                                    if (e.keyCode == 13) {
-                                        api.search(this.value).draw();
-                            }
-                        });
-                    },
-                    oLanguage: {
-                        sProcessing: "loading..."
-                    },
-                    processing: true,
-                    serverSide: true,
-                    ajax: {"url": "transaksi/json", "type": "POST"},
-                    columns: [
-                        {
-                            "data": "id_transaksi",
-                            "orderable": false
-                        },						
-                            {"data": "id_transaksi"},
-                            {"data": "id_pelanggan"},
-                            {"data": "tanggal"},
-                        {
-                            "data" : "action",
-                            "orderable": false,
-                            "className" : "text-center"
-                        }
-                    ],
-                    order: [[0, 'desc']],
-                    rowCallback: function(row, data, iDisplayIndex) {
-                        var info = this.fnPagingInfo();
-                        var page = info.iPage;
-                        var length = info.iLength;
-                        var index = page * length + (iDisplayIndex + 1);
-                        $('td:eq(0)', row).html(index);
-                    }
-                });
-            });
+                };          
         </script>
-		<!-- // Tampil Data transaksi -->
+    <!-- // Tampil Data transaksi -->
