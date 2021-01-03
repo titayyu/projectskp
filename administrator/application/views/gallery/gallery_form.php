ad78e9a79a39c8320bbe23d1689a4f313cf6bb01
habibaturohmah14 <76829968+habibaturohmah14@users.noreply.github.com>
2021-01-03 13:32:57 +0700

    Update gallery_form.php
    update6

--- administrator/application/views/gallery/gallery_form.php
+++ administrator/application/views/gallery/gallery_form.php
@@ -1,12 +1,12 @@
 <section class="content-header">
       <h1>
-        Tita Jaya
-        <small>My Customer My Number One Priority</small>
+        E-KINERJA
+        <small>Politeknik Negeri Pangkajene Kepulauan</small>
       </h1>
       <ol class="breadcrumb">
-        <li><a href="admin"><i class="fa fa-dashboard"></i> Home</a></li>
-        <li><a href="<?php echo $back ?>">Gallery</a></li>
-        <li class="active"><?php echo $button ?> Gallery</li>
+        <li><a href=""><i class="fa fa-dashboard"></i> Home</a></li>
+        <li><a href="<?php echo $back ?>">Persetujuan SKP</a></li>
+        <li class="active">Create Persetujuan SKP</li>
       </ol>
     </section>
     <!-- Main content -->
@@ -16,61 +16,178 @@
       <div class="box">        
         <div class="box-body">
 		
-		<!-- Form input dan edit Gallery-->
-		<legend><?php echo $button ?> Gallery</legend>	
-        <form action="<?php echo $action; ?>" method="post" enctype="multipart/form-data">
-		<input type="hidden" class="form-control" name="id_gallery" id="id_gallery" value="<?php echo $id_gallery; ?>" />
-		<input type="hidden" class="form-control" name="gambar" id="gambar"  value="<?php echo $gambar; ?>" />
-	    <div class="form-group">
-            <label for="varchar">Judul Gallery <?php echo form_error('judul_gallery') ?></label>
-            <input type="text" class="form-control" name="judul_gallery" id="judul_gallery" placeholder="Judul Gallery" value="<?php echo $judul_gallery; ?>" />
-        </div>	    
-		<div class="form-group">
-			<label for="varchar">Gambar <?php echo form_error('gambar') ?></label>
-			<div>
-			<?php
-				if($gambar==""){
-					echo"<p class='help-block'>Silahkan upload gambar kegiatan & lomba</p>";
-				}
-				else{
-			?>
-				<div>			
-					<img src="../../../images/gallery/<?php echo $gambar; ?>">
-				</div><br />
-			<?php
-								}
-			?>
-			<input type="file" name="gambar" id="gambar">							
-			</div>
-		</div>	   
-		<div class="form-group">
-			<label for="enum">Aktif <?php echo form_error('aktif') ?></label>
-			<select name="aktif" class="form-control select2" style="width: 100%;">		
-			<?php
-				if($aktif == 'Y'){
-			?>
-					<option value="Y" selected>Ya</option>
-					<option value="N">Tidak</option>
-			<?php
-				}
-				elseif($aktif == 'N'){
-			?>
-					<option value="Y">Ya</option>
-					<option value="N" selected>Tidak</option>
-			<?php
-				}
-				else{
-			?>
-					<option value="Y">Ya</option>
-					<option value="N">Tidak</option>
-			<?php
-				}
-			?>
-			</select>              
-		</div>
-				
-	    <button type="submit" class="btn btn-primary"><?php echo $button ?></button> 
-	    <a href="<?php echo site_url('gallery') ?>" class="btn btn-default">Cancel</a>
-	</form>
+		<div class="box-body">
+            <label class="col-sm-2" for="char">Pegawai</label>
+                <div class="col-sm-4">
+                </div>
+
+            <label class="col-sm-2" for="char">Pejabat Penilai</label>
+                <div class="col-sm-4">
+                </div>
+        </div>
+
+        <div class="box-body">
+            <div class="col-sm-2">Nama</div>           
+                <div class="col-sm-4">
+                <input type="text" class="form-control" name="id_transaksi" id="transaksi_id" placeholder="" 
+                value="Saifuddin, S.Sos.I., M.Pd" readonly/>
+                </div>
+            <div class="col-sm-2">Nama</div>
+			    <div class="col-sm-4">
+				<input type="text" class="form-control" name="id_transaksi" id="transaksi_id" placeholder="" 
+                value="Drs. Subki" readonly/>
+                </div>
+
+            <div class="col-sm-2">NIP</div>
+                <div class="col-sm-4">
+                <input type="text" class="form-control" name="id_transaksi" id="transaksi_id" placeholder="" 
+                value="196705222001121001" readonly/>
+                </div>
+            <div class="col-sm-2">NIP</div>
+                <div class="col-sm-4">
+                <input type="text" class="form-control" name="id_transaksi" id="transaksi_id" placeholder="" 
+                value="196808051993031003" readonly/>
+                </div>
+
+            <div class="col-sm-2">Pangkat/Golongan</div>
+                <div class="col-sm-4">
+                <input type="text" class="form-control" name="id_transaksi" id="transaksi_id" placeholder="" 
+                value="Penata / (IIIc)" readonly/>
+                </div>
+            <div class="col-sm-2">Pangkat/Golongan</div>
+                <div class="col-sm-4">
+                <input type="text" class="form-control" name="id_transaksi" id="transaksi_id" placeholder="" 
+                value="Pembina / (IV/a)" readonly/>
+                </div>
+
+            <div class="col-sm-2">Jabatan</div>
+                <div class="col-sm-4">
+                <input type="text" class="form-control" name="id_transaksi" id="transaksi_id" placeholder="" 
+                value="Kasubbag. Organisasi dan Tata Laksana" readonly/>
+                </div>
+            <div class="col-sm-2">Jabatan</div>
+                <div class="col-sm-4">
+                <input type="text" class="form-control" name="id_transaksi" id="transaksi_id" placeholder="" 
+                value="Kabbag. Organisasi dan Kepegawaian" readonly/>
+                </div>
+
+            <div class="col-sm-2">Unit Kerja</div>
+                <div class="col-sm-4">
+                <input type="text" class="form-control" name="id_transaksi" id="transaksi_id" placeholder="" 
+                value="Bagian Organisasi dan Kepegawaian" readonly/>
+                </div>
+            <div class="col-sm-2">Unit Kerja</div>
+                <div class="col-sm-4">
+                <input type="text" class="form-control" name="id_transaksi" id="transaksi_id" placeholder="" 
+                value="Biro AUAK Polipangkep" readonly/>
+                </div>
+      </div>
+
+        <!-- Tabel tugas jabatan -->       
+            <div class="box-body">
+            <label class="col-sm-12" for="char">Tugas Jabatan</center></label>
+
+        <div class="box-body">
+        <table class="table table-bordered table-striped" id="mytable">
+				<thead>
+					<tr>
+                        <th><center>No</th>
+                        <th><center>Kegiatan</th>
+                        <th><center>Deskripsi</th>
+                        <th><center>Angka Kredit</th>
+                        <th><center>Kuantitas</th>
+                        <th><center>Kualitas</th>
+                        <th><center>Waktu</th>
+                        <th><center>Biaya</th>
+                    </tr>
+                </thead>
+                <thead class="">
+                        <td><center>1</td>
+                        <td><center>RKT</td>
+                        <td><center>Menyusun Rencana Kegiatan Tahunan (RKT)</td>
+                        <td><center>0</td>
+                        <td><center>1 Kegiatan</td>
+                        <td><center>100</td>
+                        <td><center>1 Bulan</td>
+                        <td><center>0</td>
+                        
+                </thead>
+                <thead class="">
+                        <td><center>2</td>
+                        <td><center>RAB</td>
+                        <td><center>Menyusun Rencana Anggaran Biaya (RAB) Sub Bagian</td>
+                        <td><center>0</td>
+                        <td><center>1 Kegiatan</td>
+                        <td><center>100</td>
+                        <td><center>1 Bulan</td>
+                        <td><center>0</td>
+                        
+                </thead>
+                <thead class="">
+                        <td><center>3</td>
+                        <td><center>Pengusulan SK</td>
+                        <td><center>Melaksanakan Pengusulan SK Tugas Belajar/Izin Belajar</td>
+                        <td><center>0</td>
+                        <td><center>60 Usulan SK</td>
+                        <td><center>100</td>
+                        <td><center>6 Bulan</td>
+                        <td><center>0</td>
+                        
+                </thead>
+        </tbody>
+		</table>
+        </div>
+        </div>          
+
+    <!-- Tabel tugas tambahan -->       
+            <div class="box-body">
+            <label class="col-sm-12" for="char">Tugas Tambahan</center></label>
+
+        <div class="box-body">
+        <table class="table table-bordered table-striped" id="mytable">
+                <thead>
+                    <tr>
+                        <th><center>No</th>
+                        <th><center>Kegiatan</th>
+                        <th><center>Deskripsi</th>
+                        <th><center>Angka Kredit</th>
+                        <th><center>Kuantitas</th>
+                        <th><center>Kualitas</th>
+                        <th><center>Waktu</th>
+                        <th><center>Biaya</th>
+                    </tr>
+                </thead>
+                <thead class="">
+                        <td><center>1</td>
+                        <td><center>Anggota kegiatan</td>
+                        <td><center>Menjadi anggota dalam berbagai kegiatan diluar tupoksi</td>
+                        <td><center>0</td>
+                        <td><center>0</td>
+                        <td><center>0</td>
+                        <td><center>0</td>
+                        <td><center>0</td>
+                        
+                </thead>
+        </table>
+
+        <div class="pull-left">
+        </div>
+    </div>
+    
+    <div class="box-body">
+        <div class="pull-right">
+	    <a href="<?php echo site_url('gallery') ?>" class="btn btn-primary">Setujui</a>
+
+        </div>
+    </div>
+
+    <div class="box-body">
+        <div class="">
+            <div class="col-sm-13">
+                <input type="text" class="form-control" name="id_transaksi" id="transaksi_id" placeholder="" 
+                value="Belum Disetujui" readonly/>
+                </div> 
+    </div>
+	</form>				
     </body>
 </html>
