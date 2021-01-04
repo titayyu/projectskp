5bbf5673def5b0acd058a4f81a5e066eb2426fb1
Inayah Auliyah <Inayahauliyah97@gmail.com>
2021-01-04 17:07:51 +0700

    Update pelanggan_list.php
    update menu LKP

--- administrator/application/views/pelanggan/pelanggan_list.php
+++ administrator/application/views/pelanggan/pelanggan_list.php
@@ -1,54 +1,52 @@
 <section class="content-header">
       <h1>
         E-KINERJA
-        <small>Politeknik Negeri Pangkajenna Kepulauan</small>
+        <small>Politeknik Negeri Pangkajenne Kepulauan</small>
       </h1>
       <ol class="breadcrumb">
         <li><a href="admin"><i class="fa fa-dashboard"></i> Home</a></li>
 		<li class="active">LKP</li>
-		<li class="active">Input Data LKP</li>
+		<li class="active">Upload Data LKP</li>
       </ol>
     </section>
     <!-- Main content -->
     <section class="content">
-
       <!-- Default box -->
       <div class="box">        
         <div class="box-body">
 		
-			<!-- Menampilkan Data Pelanggan -->
-			<div class="row" style="margin-bottom: 10px">
-				<div class="col-md-4">
-					<h4 style="margin-top:0px">Amriliani Hambali S.Pd, M.Pd</h4>
-				</div>
-				<!-- Menampilkan Data Pegawai -->
+			<!-- Menampilkan Data Pegawai -->
+			<div class="col-md-4">
+				<h3 style="margin-top:0px">Amriliani Hambali S.Pd, M.Pd</h3>
+			</div>
+		<div>
 			<table class="table table-bordered table-striped" id="">
 				<tr>
-						<th width="50px"><center>No</center></th>
+						<th width="50px">No</th>
 						<th >PEJABAT PENILAI</th>
-						<th width="50px"><center>No</center></th>
+						<th width="50px">No</th>
 						<th >PEGAWAI YANG DINILAI</th>	
 				</tr>
 				<tr>
-						<td width="50px"><center>1</center></td>
+						<td width="50px">1</td>
 						<td >Nama&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
 						&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
 						&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
 						&nbsp;&nbsp;&nbsp;&nbsp;:&nbsp;Ir. Miss Rahmayanti, M.Si</td>
-						<td width="50px"><center>1</center></td>
+						<td width="50px">1</td>
 						<td >Nama&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
 						&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
 						&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
 						&nbsp;&nbsp;&nbsp;&nbsp;:&nbsp;Amriliani Hambali S.Pd, M.Pd</td>
 				</tr>
 				<tr>
-						<td width="50px"><center>2</center></td>
+						<td width="50px">2</td>
 						<td >NIP&nbsp;&nbsp;&nbsp;&nbsp;
 						&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
 						&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
 						&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
 						&nbsp;&nbsp;&nbsp;&nbsp;:&nbsp;198106252014041001</td>
-						<td width="50px"><center>2</center></td>
+						<td width="50px">2</td>
 						<td >NIP&nbsp;&nbsp;&nbsp;&nbsp;
 						&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
 						&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
@@ -56,65 +54,144 @@
 						&nbsp;&nbsp;&nbsp;&nbsp;:&nbsp;197209181999031005</td>
 				</tr>
 				<tr>
-						<td width="50px"><center>3</center></td>
+						<td width="50px">3</td>
 						<td >Pangkat/Golongan&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;:&nbsp;Penata Gol.III/C</td>
-						<td width="50px"><center>3</center></td>
+						<td width="50px">3</td>
 						<td >Pangkat/Golongan&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;:&nbsp;Pengatur Tingkat I Gol.II/d</td>
 				</tr>
 				<tr>
-						<td width="50px"><center>4</center></td>
+						<td width="50px">4</td>
 						<td >Jabatan&nbsp;&nbsp;&nbsp;&nbsp;
 						&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
 						&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
 						&nbsp;&nbsp;&nbsp;&nbsp;:&nbsp;Ketua Jurusan Budidaya Tanaman Perkebunan</td>
-						<td width="50px"><center>4</center></td>
+						<td width="50px">4</td>
 						<td >Jabatan&nbsp;&nbsp;&nbsp;&nbsp;
 						&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
 						&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
 						&nbsp;&nbsp;&nbsp;:&nbsp;Pengelola Lab. Tanaman</td>
 				</tr>
 				<tr>
-						<td width="50px"><center>5</center></td>
+						<td width="50px">5</td>
 						<td >Unit Kerja&nbsp;&nbsp;&nbsp;&nbsp;
 						&nbsp;&nbsp;&nbsp;&nbsp;
 						&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
 						&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;:&nbsp;Budidaya Tanaman Perkebunan</td>
-						<td width="50px"><center>5</center></td>
+						<td width="50px">5</td>
 						<td >Unit Kerja&nbsp;&nbsp;&nbsp;&nbsp;
 						&nbsp;&nbsp;&nbsp;&nbsp;
 						&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
 						&nbsp;&nbsp;&nbsp;&nbsp;:&nbsp;Budidaya Tanaman Perkebunan</td>
 				</tr>
 			</table>
-			<!--	<div class="col-md-4 text-center">
-					<div style="margin-top: 4px"  id="message">
-						<?php echo $this->session->userdata('message') <> '' ? $this->session->userdata('message') : ''; ?>
-					</div>
-				</div> -->
-			</div>
-			<table class="table table-bordered table-striped" id="mytable">
-				<thead>
-					<tr>
-						<th width="80px"><center>No</center></th>
-						<th><center>Kegiatan Jabatan</center></th>
-						<th></th>
-						<th><center>Kuatitas</center></th>
-						<th width="200px"><center>Action</center></th>					
-						<th><center>Tanggal Upload</center></th>
-					</tr>
-				</thead>
-			
+		</div>
+			<table class="table table-bordered table-striped" id="">
+				<tr>
+					<th width="50px"><center>No</center></th> 
+					<th><center>Kegiatan Jabatan</center></th>  
+					<th width="100px"><center>Input Berkas</center></th>               
+                    <th width="140px"></th>
+                    <th><center>Kuantitas</center></th>
+                    <th width="150px"><center>Aksi</center></th>
+					<th><center>Tanggal_Upload</center></th>
+				</tr>
+				<tr>
+					<td><center>1</center></td>
+					<td>Laporan Pengabdian Masyarakat</td>
+					<td><input type="file" name="nama_file"></td>
+					<td><a href="<?php echo site_url('pelanggan') ?>"class="btn btn-primary" onclick="notifikasi()">Upload</a>&nbsp;<a href="<?php echo site_url('transaksi') ?>" class="btn btn-primary">Edit</a></td>
+					<td><center>1 Document</center></td>
+					<td><center><a class="btn btn-sm btn-success" data-toggle="modal" data-target="#modal_add_new">Berhasil Upload</a></center></td>
+					<td><center>17/07/2020</center></td>
+				</tr>
+				<tr>
+					<td><center>2</center></td>
+					<td>SK Mengajar</td>
+					<td><input type="file" name="nama_file"></td>
+					<td><button type="submit" class="btn btn-primary">Upload</button>&nbsp;<a href="<?php echo site_url('transaksi') ?>" class="btn btn-primary">Edit</a></td>
+					<td><center>1 Document</center></td>
+					<td><center><a class="btn btn-sm btn-success" data-toggle="modal" data-target="#modal_add_new">Berhasil Upload</a></center></td>
+					<td><center>17/07/2020</center></td>
+				</tr>
+				<tr>
+					<td><center>3</center></td>
+					<td>SK Direktur</td>
+					<td><input type="file" name="nama_file"></td>
+					<td><button type="submit" class="btn btn-primary">Upload</button>&nbsp;<a href="<?php echo site_url('transaksi') ?>" class="btn btn-default">Edit</a></td>
+					<td><center>0 Document</center></td>
+					<td><center><a class="btn btn-sm btn-primary" data-toggle="modal" data-target="#modal_add_new">Gagal Upload</a></center></td>
+					<td><center>17/07/2020</center></td>
+				</tr>
+				<tr>
+					<td><center>4</center></td>
+					<td>Jurnal</td>
+					<td><input type="file" name="nama_file"></td>
+					<td><button type="submit" class="btn btn-primary">Upload</button>&nbsp;<a href="<?php echo site_url('transaksi') ?>" class="btn btn-default">Edit</a></td>
+					<td><center>0 Document</center></td>
+					<td><center><a class="btn btn-sm btn-default" data-toggle="modal" data-target="#modal_add_new">Belum Upload</a></center></td>
+					<td><center>17/07/2020</center></td>
+				</tr>
+				<tr>
+					<td><center>5</center></td>
+					<td>Sertifikat</td>
+					<td><input type="file" name="nama_file"></td>
+					<td><button type="submit" class="btn btn-primary">Upload</button>&nbsp;<a href="<?php echo site_url('transaksi') ?>" class="btn btn-default">Edit</a></td>
+					<td><center>0 Document</center></td>
+					<td><center><a class="btn btn-sm btn-default" data-toggle="modal" data-target="#modal_add_new">Belum Upload</a></center></td>
+					<td><center>17/07/2020</center></td>
+				</tr>
 			</table>
-			<div class="col-md-4 text-right">
-				<?php if($_SESSION['level'] == 'manager'){}else{echo anchor(site_url('pelanggan/create'), '<i class="fa fa-file-text" aria-hidden="true"></i>&nbsp;Create', 'class="btn btn-primary"'); }?>
-			</div>
-			<!-- Memanggil jQuery -->
-			<script src="<?php echo base_url('assets/js/jquery-1.11.2.min.js') ?>"></script> 
-			<!-- Memanggil jQuery data tables -->
-		<!-- <script src="<?php echo base_url('assets/datatables/jquery.dataTables.js') ?>"></script> -->
-			<!-- Memanggil Bootstrap data tables -->
-		<!--	<script src="<?php echo base_url('assets/datatables/dataTables.bootstrap.js') ?>"></script> -->
+			<!-- button tambah data -->
+			<a class="btn btn-sm btn-success" data-toggle="modal" data-target="#modal_add_new"> + Tambah Document</a>
+			<!-- CSS untuk Button -->
+			<style>
+				.btn_blue{
+					background-color: blue;
+				}
 			
+			</style>
+			<br><br><br><br><br><br>
+			<div>
+			<center>&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;
+			&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;
+			<?php if($_SESSION['level'] == 'manager'){}else{echo anchor(site_url('pelanggan/create'), 'Simpan Document', 'class="btn btn-success"');} ?></center>
+			</div>
+			<br><br>
+			<!-- Memanggil jQuery data tables -->
+			<script src="<?php echo base_url('assets/datatables/jquery.dataTables.js') ?>"></script>
+			<!-- Memanggil Bootstrap data tables -->
+			<script src="<?php echo base_url('assets/datatables/dataTables.bootstrap.js') ?>"></script>
+			  <!-- Jquery -->
+			  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+         
+		 <!-- Notifikasi Script -->
+		 <script>
+			 $(document).ready(function() {
+				   if (Notification.permission !== "granted")
+					 Notification.requestPermission();
+			 });
+			  
+			 function notifikasi() {
+				 if (!Notification) {
+					 alert('Browsermu tidak mendukung Web Notification.'); 
+					 return;
+				 }
+				 if (Notification.permission !== "granted")
+					 Notification.requestPermission();
+				 else {
+					 var notifikasi = new Notification('Judul Notifikasi', {
+						 icon: 'http://jagocoding.com/theme/New/img/logo.png',
+						 body: "Belajar di Jago Coding, Sangat Menyenangkan !",
+					 });
+					 notifikasi.onclick = function () {
+						 window.open("http://goo.gl/dIf4s1");      
+					 };
+					 setTimeout(function(){
+						 notifikasi.close();
+					 }, 5000);
+				 }
+			 };
+		 </script>
 			<!-- JavaScript yang berfungsi untuk menampilkan data dari tabel pelanggan dengan AJAX -->
 			<script type="text/javascript">
 				$(document).ready(function() {
