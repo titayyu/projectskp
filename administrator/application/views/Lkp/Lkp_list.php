5c5d07298922d50295b27128f335b10a580458fc
Inayah Auliyah <Inayahauliyah97@gmail.com>
2021-01-05 22:09:04 +0700

    Create Lkp_list.php

---
+++ administrator/application/views/Lkp/Lkp_list.php
@@ -0,0 +1,248 @@
+<section class="content-header">
+      <h1>
+        E-KINERJA
+        <small>Politeknik Negeri Pangkajenne Kepulauan</small>
+      </h1>
+      <ol class="breadcrumb">
+        <li><a href="admin"><i class="fa fa-dashboard"></i> Home</a></li>
+		<li class="active">LKP</li>
+		<li class="active">Upload Data LKP</li>
+      </ol>
+    </section>
+    <!-- Main content -->
+    <section class="content">
+      <!-- Default box -->
+      <div class="box">        
+        <div class="box-body">
+		
+			<!-- Menampilkan Data Pegawai -->
+			<div class="col-md-4">
+				<h3 style="margin-top:0px">Amriliani Hambali S.Pd, M.Pd</h3>
+			</div>
+		<div>
+			<table class="table table-bordered table-striped" id="">
+				<tr>
+						<th width="50px">No</th>
+						<th >PEJABAT PENILAI</th>
+						<th width="50px">No</th>
+						<th >PEGAWAI YANG DINILAI</th>	
+				</tr>
+				<tr>
+						<td width="50px">1</td>
+						<td >Nama&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+						&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+						&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+						&nbsp;&nbsp;&nbsp;&nbsp;:&nbsp;Ir. Miss Rahmayanti, M.Si</td>
+						<td width="50px">1</td>
+						<td >Nama&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+						&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+						&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+						&nbsp;&nbsp;&nbsp;&nbsp;:&nbsp;Amriliani Hambali S.Pd, M.Pd</td>
+				</tr>
+				<tr>
+						<td width="50px">2</td>
+						<td >NIP&nbsp;&nbsp;&nbsp;&nbsp;
+						&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+						&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+						&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+						&nbsp;&nbsp;&nbsp;&nbsp;:&nbsp;198106252014041001</td>
+						<td width="50px">2</td>
+						<td >NIP&nbsp;&nbsp;&nbsp;&nbsp;
+						&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+						&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+						&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+						&nbsp;&nbsp;&nbsp;&nbsp;:&nbsp;197209181999031005</td>
+				</tr>
+				<tr>
+						<td width="50px">3</td>
+						<td >Pangkat/Golongan&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;:&nbsp;Penata Gol.III/C</td>
+						<td width="50px">3</td>
+						<td >Pangkat/Golongan&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;:&nbsp;Pengatur Tingkat I Gol.II/d</td>
+				</tr>
+				<tr>
+						<td width="50px">4</td>
+						<td >Jabatan&nbsp;&nbsp;&nbsp;&nbsp;
+						&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+						&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+						&nbsp;&nbsp;&nbsp;&nbsp;:&nbsp;Ketua Jurusan Budidaya Tanaman Perkebunan</td>
+						<td width="50px">4</td>
+						<td >Jabatan&nbsp;&nbsp;&nbsp;&nbsp;
+						&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+						&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+						&nbsp;&nbsp;&nbsp;:&nbsp;Pengelola Lab. Tanaman</td>
+				</tr>
+				<tr>
+						<td width="50px">5</td>
+						<td >Unit Kerja&nbsp;&nbsp;&nbsp;&nbsp;
+						&nbsp;&nbsp;&nbsp;&nbsp;
+						&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+						&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;:&nbsp;Budidaya Tanaman Perkebunan</td>
+						<td width="50px">5</td>
+						<td >Unit Kerja&nbsp;&nbsp;&nbsp;&nbsp;
+						&nbsp;&nbsp;&nbsp;&nbsp;
+						&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+						&nbsp;&nbsp;&nbsp;&nbsp;:&nbsp;Budidaya Tanaman Perkebunan</td>
+				</tr>
+			</table>
+		</div>
+			<table class="table table-bordered table-striped" id="">
+				<tr>
+					<th width="50px"><center>No</center></th> 
+					<th><center>Kegiatan Jabatan</center></th>  
+					<th width="100px"><center>Input Berkas</center></th>               
+                    <th><center>Kuantitas</center></th>
+                    <th width="150px"><center>Aksi</center></th>
+					<th><center>Tanggal_Upload</center></th>
+				</tr>
+				<tr>
+					<td><center>1</center></td>
+					<td>Laporan Pengabdian Masyarakat</td>
+					<td><input type="file" name="nama_file"></td>
+					<td><center>1 Document</center></td>
+					<td><center><a class="btn btn-sm btn-success" data-toggle="modal" data-target="#modal_add_new">Berhasil Upload</a></center></td>
+					<td><center>17/07/2020</center></td>
+				</tr>
+				<tr>
+					<td><center>2</center></td>
+					<td>SK Mengajar</td>
+					<td><input type="file" name="nama_file"></td>
+					<td><center>1 Document</center></td>
+					<td><center><a class="btn btn-sm btn-success" data-toggle="modal" data-target="#modal_add_new">Berhasil Upload</a></center></td>
+					<td><center>17/07/2020</center></td>
+				</tr>
+				<tr>
+					<td><center>3</center></td>
+					<td>SK Direktur</td>
+					<td><input type="file" name="nama_file"></td>
+					<td><center>1 Document</center></td>
+					<td><center><a class="btn btn-sm btn-success" data-toggle="modal" data-target="#modal_add_new">Berhasil Upload</a></center></td>
+					<td><center>17/07/2020</center></td>
+				</tr>
+				<tr>
+					<td><center>4</center></td>
+					<td>Jurnal</td>
+					<td><input type="file" name="nama_file"></td>
+					<td><center>1 Document</center></td>
+					<td><center><a class="btn btn-sm btn-success" data-toggle="modal" data-target="#modal_add_new">Berhasil Upload</a></center></td>
+					<td><center>17/07/2020</center></td>
+				</tr>
+				<tr>
+					<td><center>5</center></td>
+					<td>Sertifikat</td>
+					<td><input type="file" name="nama_file"></td>
+					<td><center>1 Document</center></td>
+					<td><center><a class="btn btn-sm btn-success" data-toggle="modal" data-target="#modal_add_new">Berhasil Upload</a></center></td>
+					<td><center>17/07/2020</center></td>
+				</tr>
+			</table>
+			<!-- button tambah data -->
+			<a class="btn btn-sm btn-success" data-toggle="modal" data-target="#modal_add_new"> + Tambah Document</a>
+			<!-- CSS untuk Button -->
+			<style>
+				.btn_blue{
+					background-color: blue;
+				}
+			
+			</style>
+			<br><br><br><br><br><br>
+			<div>
+			<center>&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;
+			&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;
+			<?php if($_SESSION['level'] == 'manager'){}else{echo anchor(site_url('Lkp/create'), 'Simpan Document', 'class="btn btn-success"');} ?></center>
+			</div>
+			<br><br>
+			<!-- Memanggil jQuery data tables -->
+			<script src="<?php echo base_url('assets/datatables/jquery.dataTables.js') ?>"></script>
+			<!-- Memanggil Bootstrap data tables -->
+			<script src="<?php echo base_url('assets/datatables/dataTables.bootstrap.js') ?>"></script>
+			  <!-- Jquery -->
+			  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+         
+		 <!-- Notifikasi Script -->
+		 <script>
+			 $(document).ready(function() {
+				   if (Notification.permission !== "granted")
+					 Notification.requestPermission();
+			 });
+			  
+			 function notifikasi() {
+				 if (!Notification) {
+					 alert('Browsermu tidak mendukung Web Notification.'); 
+					 return;
+				 }
+				 if (Notification.permission !== "granted")
+					 Notification.requestPermission();
+				 else {
+					 var notifikasi = new Notification('Judul Notifikasi', {
+						 icon: 'http://jagocoding.com/theme/New/img/logo.png',
+						 body: "Belajar di Jago Coding, Sangat Menyenangkan !",
+					 });
+					 notifikasi.onclick = function () {
+						 window.open("http://goo.gl/dIf4s1");      
+					 };
+					 setTimeout(function(){
+						 notifikasi.close();
+					 }, 5000);
+				 }
+			 };
+		 </script>
+			<!-- JavaScript yang berfungsi untuk menampilkan data dari tabel pelanggan dengan AJAX -->
+			<script type="text/javascript">
+				$(document).ready(function() {
+					$.fn.dataTableExt.oApi.fnPagingInfo = function(oSettings)
+					{
+						return {
+							"iStart": oSettings._iDisplayStart,
+							"iEnd": oSettings.fnDisplayEnd(),
+							"iLength": oSettings._iDisplayLength,
+							"iTotal": oSettings.fnRecordsTotal(),
+							"iFilteredTotal": oSettings.fnRecordsDisplay(),
+							"iPage": Math.ceil(oSettings._iDisplayStart / oSettings._iDisplayLength),
+							"iTotalPages": Math.ceil(oSettings.fnRecordsDisplay() / oSettings._iDisplayLength)
+						};
+					};
+					
+					var t = $("#mytable").dataTable({
+						initComplete: function() {
+							var api = this.api();
+							$('#mytable_filter input')
+									.off('.DT')
+									.on('keyup.DT', function(e) {
+										if (e.keyCode == 13) {
+											api.search(this.value).draw();
+										}
+							});
+						},
+						oLanguage: {
+							sProcessing: "loading..."
+						},
+						processing: true,
+						serverSide: true,
+						ajax: {"url": "pelanggan/json", "type": "POST"},
+						columns: [
+							{
+								"data": "id_pelanggan",
+								"orderable": false
+							},
+								{"data": "id_pelanggan"},
+								{"data": "nama"},
+								{"data": "alamat"},
+								{"data": "telp"},
+							{
+								"data" : "action",
+								"orderable": false,
+								"className" : "text-center"
+							}
+						],
+						order: [[0, 'desc']],
+						rowCallback: function(row, data, iDisplayIndex) {
+							var info = this.fnPagingInfo();
+							var page = info.iPage;
+							var length = info.iLength;
+							var index = page * length + (iDisplayIndex + 1);
+							$('td:eq(0)', row).html(index);
+						}
+					});
+				});
+			</script>
+		<!--// Tampil Data Pelanggan -->    
