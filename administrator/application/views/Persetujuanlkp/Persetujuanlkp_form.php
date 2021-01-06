a480c9e9b1813cfd94d0ddbd64224b6aaf33362e
Inayah Auliyah <Inayahauliyah97@gmail.com>
2021-01-06 13:41:50 +0700

    Create Persetujuanlkp_form.php

---
+++ administrator/application/views/Persetujuanlkp/Persetujuanlkp_form.php
@@ -0,0 +1,208 @@
+<section class="content-header">
+      <h1>
+        E-KINERJA
+        <small>Politeknik Negeri Pangkajenne Kepulauan</small>
+      </h1>
+      <ol class="breadcrumb">
+        <li><a href="admin"><i class="fa fa-dashboard"></i> Home</a></li>
+        <li><a href="<?php echo $back ?>">Daftar Pengajuan LKP</a></li>
+        <li class="active"> Penilaian dan Persetujuan LKP</li>
+      </ol>
+    </section>
+    <!-- Main content -->
+    <section class="content">
+
+      <!-- Default box -->
+      <div class="box">        
+        <div class="box-body">
+		<table class="table table-bordered table-striped" id="">
+				<tr>
+						<th width="30px">No</th>
+						<th >PEJABAT PENILAI</th>
+						<th width="30px">No</th>
+						<th >PEGAWAI YANG DINILAI</th>	
+				</tr>
+				<tr>
+						<td width="30px"><center>1</center></td>
+						<td >Nama&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+						&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+						&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+						&nbsp;&nbsp;&nbsp;&nbsp;:&nbsp;Saifuddin, S.Sos.I.,M.Pd</td>
+						<td width="30px"><center>1</center></td>
+						<td >Nama&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+						&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+						&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+						&nbsp;&nbsp;&nbsp;&nbsp;:&nbsp;Drs. Subki</td>
+				</tr>
+				<tr>
+						<td width="30px"><center>2</center></td>
+						<td >NIP&nbsp;&nbsp;&nbsp;&nbsp;
+						&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+						&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+						&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+						&nbsp;&nbsp;&nbsp;&nbsp;:&nbsp;196705222001121001</td>
+						<td width="30px"><center>2</center></td>
+						<td >NIP&nbsp;&nbsp;&nbsp;&nbsp;
+						&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+						&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+						&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+						&nbsp;&nbsp;&nbsp;&nbsp;:&nbsp;196808051993031003</td>
+				</tr>
+				<tr>
+						<td width="30px"><center>3</center></td>
+						<td >Pangkat/Golongan&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;:&nbsp;Penata / (IIIc)</td>
+						<td width="30px"><center>3</center></td>
+						<td >Pangkat/Golongan&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;:&nbsp;Pengatur / (IV/a)</td>
+				</tr>
+				<tr>
+						<td width="30px"><center>4</center></td>
+						<td >Jabatan&nbsp;&nbsp;&nbsp;&nbsp;
+						&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+						&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+						&nbsp;&nbsp;&nbsp;&nbsp;:&nbsp;Kasubbag. Organisasi dan Tata Laksana</td>
+						<td width="30px"><center>4</center></td>
+						<td >Jabatan&nbsp;&nbsp;&nbsp;&nbsp;
+						&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+						&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+						&nbsp;&nbsp;&nbsp;:&nbsp;Kasubbag. Organisasi dan Kepegawaian</td>
+				</tr>
+				<tr>
+						<td width="30px"><center>5</center></td>
+						<td >Unit Kerja&nbsp;&nbsp;&nbsp;&nbsp;
+						&nbsp;&nbsp;&nbsp;&nbsp;
+						&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+						&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;:&nbsp;Bagian Organisasi dan Kepegawaian</td>
+						<td width="30px"><center>5</center></td>
+						<td >Unit Kerja&nbsp;&nbsp;&nbsp;&nbsp;
+						&nbsp;&nbsp;&nbsp;&nbsp;
+						&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+						&nbsp;&nbsp;&nbsp;&nbsp;:&nbsp;Biro BAUK Polipangkep</td>
+				</tr>
+			</table>
+		</div>
+		<div>
+		<label>Tugas Jabatan</label>
+			<table class="table table-bordered table-striped" id="">
+			<tr>
+					<th><center></center></th> 
+					<th><center></center></th>
+					<th><center></center></th>               
+					<th><center></center></th>
+					<th><center>Target</center></th>  
+                    <th><center></center></th>
+					<th><center></center></th>
+					<th><center></center></th>               
+					<th><center></center></th>
+					<th><center>Realisasi</center></th>  
+                    <th><center></center></th>
+					<th><center></center></th>
+				</tr>
+				<tr>
+					<th width="30px"><center>No</center></th> 
+					<th><center>Kegiatan Jabatan</center></th>
+					<th><center>AK</center></th>               
+					<th><center>Kuantitas</center></th>
+					<th><center>Kualitas</center></th>  
+                    <th><center>Waktu</center></th>
+					<th><center>Biaya</center></th>
+					<th><center>AK</center></th>               
+					<th><center>Kuantitas</center></th>
+					<th><center>Kualitas</center></th>  
+                    <th><center>Waktu</center></th>
+					<th><center>Biaya</center></th>
+				</tr>
+				<tr>
+					<td><center>1</center></td>
+					<td>Menyusun Kegiatan Rencana Tahunan (RKT)</td>
+					<td><center>0</center></td>
+					<td><center>1 Document</center></td>
+					<td><center>100</center></td>
+					<td><center>1 Bulan</center></td>
+					<td><center>0</center></td>
+					<td><center>0</center></td>
+					<td><center>1 Document</center></td>
+					<td><center>100</center></td>
+					<td><center>3 Bulan</center></td>
+					<td><center>0</center></td>
+				</tr>
+				<tr>
+					<td><center>2</center></td>
+					<td>Menyusun Rencana Anggaran Biaya (RAB) Sub Bagian</td>
+					<td><center>0</center></td>
+					<td><center>1 Document</center></td>
+					<td><center>100</center></td>
+					<td><center>1 Bulan</center></td>
+					<td><center>0</center></td>
+					<td><center>0</center></td>
+					<td><center>1 Document</center></td>
+					<td><center>100</center></td>
+					<td><center>3 Bulan</center></td>
+					<td><center>0</center></td>
+				</tr>
+				</tr>
+				<tr>
+					<td><center>3</center></td>
+					<td>Melaksanakan Pengusulan SK Tugas Belajar/Izin Belajar</td>
+					<td><center>0</center></td>
+					<td><center>1 Document</center></td>
+					<td><center>100</center></td>
+					<td><center>3 Bulan</center></td>
+					<td><center>0</center></td>
+					<td><center>0</center></td>
+					<td><center>1 Document</center></td>
+					<td><center>100</center></td>
+					<td><center>6 Bulan</center></td>
+					<td><center>0</center></td>
+				</tr>
+				</tr>
+			</table>
+			<table class="table table-bordered table-striped" id="">
+			<tr>
+					<th><center></center></th> 
+					<th><center></center></th>
+					<th><center></center></th>               
+					<th><center></center></th>
+					<th><center>Target</center></th>  
+                    <th><center></center></th>
+					<th><center></center></th>
+					<th><center></center></th>               
+					<th><center></center></th>
+					<th><center>Realisasi</center></th>  
+                    <th><center></center></th>
+					<th><center></center></th>
+				</tr>
+				<tr>
+					<th width="30px"><center>No</center></th> 
+					<th><center>Kegiatan Jabatan Tambahan</center></th>
+					<th><center>AK</center></th>               
+					<th><center>Kuantitas</center></th>
+					<th><center>Kualitas</center></th>  
+                    <th><center>Waktu</center></th>
+					<th><center>Biaya</center></th>
+					<th><center>AK</center></th>               
+					<th><center>Kuantitas</center></th>
+					<th><center>Kualitas</center></th>  
+                    <th><center>Waktu</center></th>
+					<th><center>Biaya</center></th>
+				</tr>
+				<tr>
+					<td><center>1</center></td>
+					<td>Menjadi Anggota dalam Berbagai kegiatan Diluar Tupoksi</td>
+					<td><center>0</center></td>
+					<td><center>1 Document</center></td>
+					<td><center>100</center></td>
+					<td><center>1 Bulan</center></td>
+					<td><center>0</center></td>
+					<td><center>0</center></td>
+					<td><center>1 Document</center></td>
+					<td><center>100</center></td>
+					<td><center>3 Bulan</center></td>
+					<td><center>0</center></td>
+				</tr>
+			</table>
+		</div>	<br><br>
+        <form role="form" action="<?php echo $action; ?>" method="post" enctype="multipart/form-data">		
+	   <center> <a href="<?php echo site_url('Persetujuanlkp') ?>" class="btn btn-primary">Setujui</a>
+	    <a href="<?php echo site_url('Persetujuanlkp') ?>" class="btn btn-default">Cancel</a> </center><br><br>
+	</form>
+    
