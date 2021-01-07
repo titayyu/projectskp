92dd1fdd2f481b3ac3ac33d7c83d0ea041662e84
Tita Ayu R <47605688+titayyu@users.noreply.github.com>
2021-01-07 21:22:12 +0700

    Update beranda.php
    tita update

--- administrator/application/views/beranda.php
+++ administrator/application/views/beranda.php
@@ -245,7 +245,7 @@
 				</div>
 				<!-- /.col -->
 				<div class="col-sm-4 col-md-2">
-				  <h4 class="text-center"><span class="info-box-text"><b>PENGAJUAN SURAT PERMOHONAN<b></span></h4>
+				  <h4 class="text-center"><span class="info-box-text"><b>APPROVE SURAT PERMOHONAN<b></span></h4>
 				  <div class="color-palette-set">
 					<a href="<?php echo site_url('informasi') ?>"><center><i class="fa fa-file-text-o" style="font-size:48px;color:#3c8dbc"></i><center></a>
 				  </div>
